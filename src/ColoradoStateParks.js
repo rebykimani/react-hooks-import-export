82bc76f3d934d9dbc83b95f76602bab340788d02
beckireby15@gmail.com <beckireby15@gmail.com>
2022-09-20 02:02:36 +0300

    utilized export and imports

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,10 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
 
-function ColoradoStateParks() {
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+// export default ColoradoStateParks();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+// ReactDOM.render();
+function App() {
+  return (<ColoradoStateParks />), document.getElementById("root");
+}
+export default App;
